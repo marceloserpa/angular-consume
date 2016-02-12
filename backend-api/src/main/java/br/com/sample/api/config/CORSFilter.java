4c191eea6af1fb4ebe6aca67fc345a0df9f2861c
MarceloIlegra <marcelo.serpa@ilegra.com>
2016-02-12 19:06:51 -0200

    configurated CORS filter for ajax calls

---
+++ backend-api/src/main/java/br/com/sample/api/config/CORSFilter.java
@@ -0,0 +1,39 @@
+package br.com.sample.api.config;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+
+@Component
+public class CORSFilter implements Filter {
+ 
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {}
+ 
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response=(HttpServletResponse) resp;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        chain.doFilter(req, resp);
+    }
+ 
+    @Override
+    public void destroy() {}
+ 
+}
