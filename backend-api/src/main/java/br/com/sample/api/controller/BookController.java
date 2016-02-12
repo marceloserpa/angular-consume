fcb0863419b399f8c30686a0a3f1e47614fbb124
MarceloIlegra <marcelo.serpa@ilegra.com>
2016-02-12 19:14:23 -0200

    calling localhost api

--- backend-api/src/main/java/br/com/sample/api/controller/BookController.java
+++ backend-api/src/main/java/br/com/sample/api/controller/BookController.java
@@ -6,15 +6,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.sample.api.model.BookModel;
+
 @RestController
 public class BookController {
 
 	@RequestMapping(method=RequestMethod.GET, value="api/books")
-	public ArrayList<String> getBooksName(){
-		ArrayList<String> list = new ArrayList<>();
-		list.add("Book A");
-		list.add("Book B");
-		list.add("Book C");
+	public ArrayList<BookModel> getBooksName(){
+		ArrayList<BookModel> list = new ArrayList<>();
+		BookModel book1 = new BookModel();
+		book1.setTitle("Title 1");
+		book1.setDescription("Description 1");
+		book1.setAuthor("Author 1");
+		list.add(book1);
+		
+		BookModel book2 = new BookModel();
+		book2.setTitle("Title 2");
+		book2.setDescription("Description 2");
+		book2.setAuthor("Author 2");
+		list.add(book2);
+		
+		BookModel book3 = new BookModel();
+		book3.setTitle("Title 3");
+		book3.setDescription("Description 3");
+		book3.setAuthor("Author 3");
+		list.add(book3);
+		
 		return list;
 	}
 	
