76b12b57c7480de22999b4a87575cf2c75910170
MarceloIlegra <marcelo.serpa@ilegra.com>
2016-02-17 12:38:56 -0200

    Separate BookController form App definition

---
+++ frontend/src/app/books/BookController.js
@@ -0,0 +1,53 @@
+bookStoreApp.controller('BookController', function ($scope, $http) {
+
+  $scope.editForm = {};
+
+  $scope.get = function(){
+    $http.get("http://localhost:8080/api/books").success(function(data) {
+      $scope.books = data;
+    });
+  };
+
+  $scope.save = function(){
+    var book = $scope.createForm;
+    $http.post('http://localhost:8080/api/books', book).success(function(){
+      $scope.get();
+      $scope.cleanForm();
+    });
+  };
+
+  $scope.delete = function(id){
+    $http.delete('http://localhost:8080/api/books/' + id).success(function(){
+      $scope.get();
+    });
+  };
+
+  $scope.update = function(){
+    var book = $scope.editForm;
+    $http.put('http://localhost:8080/api/books/'+ book.id, book).success(function(){
+      $scope.get();
+      $scope.editForm = {
+        'id': '',
+        'title': '',
+        'description': '',
+        'author': ''
+      };
+    });
+  };
+
+  $scope.initEditForm = function(id){
+    $http.get("http://localhost:8080/api/books/"+id).success(function(data) {
+      $scope.editForm = data;
+    });
+  };
+
+  $scope.cleanForm = function(){
+    $scope.createForm = {
+      'title': '',
+      'description': '',
+      'author': ''
+    };
+  };
+
+  $scope.get();
+});
