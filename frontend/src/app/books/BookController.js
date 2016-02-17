9069ac19d39efadb53ab781f9223db64b70ee32d
MarceloIlegra <marcelo.serpa@ilegra.com>
2016-02-17 16:01:21 -0200

    removing global variable

--- frontend/src/app/books/BookController.js
+++ frontend/src/app/books/BookController.js
@@ -1,70 +1,72 @@
+(function(){
+  'use strict';
+  
+  angular.module('bookStoreApp').controller('BookController', ['$scope', '$http', '$routeParams', 'action', function ($scope, $http, $routeParams, action) {
 
-bookStoreApp.controller('BookController', ['$scope', '$http', '$routeParams', 'action', function ($scope, $http, $routeParams, action) {
+    $scope.editForm = {};
 
-  $scope.editForm = {};
+    $scope.get = function(){
+      $http.get("http://localhost:8080/api/books").success(function(data) {
+        $scope.books = data;
+      });
+    };
 
-  $scope.get = function(){
-    $http.get("http://localhost:8080/api/books").success(function(data) {
-      $scope.books = data;
-    });
-  };
+    $scope.save = function(){
+      var book = $scope.createForm;
+      $http.post('http://localhost:8080/api/books', book).success(function(){
+        $scope.get();
+        $scope.cleanForm();
+      });
+    };
 
-  $scope.save = function(){
-    var book = $scope.createForm;
-    $http.post('http://localhost:8080/api/books', book).success(function(){
-      $scope.get();
-      $scope.cleanForm();
-    });
-  };
+    $scope.delete = function(id){
+      $http.delete('http://localhost:8080/api/books/' + id).success(function(){
+        $scope.get();
+      });
+    };
 
-  $scope.delete = function(id){
-    $http.delete('http://localhost:8080/api/books/' + id).success(function(){
-      $scope.get();
-    });
-  };
+    $scope.update = function(){
+      var book = $scope.editForm;
+      $http.put('http://localhost:8080/api/books/'+ book.id, book).success(function(){
+        $scope.get();
+        $scope.editForm = {
+          'id': '',
+          'title': '',
+          'description': '',
+          'author': ''
+        };
+      });
+    };
 
-  $scope.update = function(){
-    var book = $scope.editForm;
-    $http.put('http://localhost:8080/api/books/'+ book.id, book).success(function(){
-      $scope.get();
-      $scope.editForm = {
-        'id': '',
+    $scope.initEditForm = function(id){
+      $http.get("http://localhost:8080/api/books/"+id).success(function(data) {
+        $scope.editForm = data;
+      });
+    };
+
+    $scope.initShowPage = function(id){
+      $http.get("http://localhost:8080/api/books/"+id).success(function(data) {
+        $scope.book = data;
+      });
+    };
+
+    $scope.cleanForm = function(){
+      $scope.createForm = {
         'title': '',
         'description': '',
         'author': ''
       };
-    });
-  };
-
-  $scope.initEditForm = function(id){
-    $http.get("http://localhost:8080/api/books/"+id).success(function(data) {
-      $scope.editForm = data;
-    });
-  };
-
-  $scope.initShowPage = function(id){
-    $http.get("http://localhost:8080/api/books/"+id).success(function(data) {
-      $scope.book = data;
-    });
-  };
-
-  $scope.cleanForm = function(){
-    $scope.createForm = {
-      'title': '',
-      'description': '',
-      'author': ''
     };
-  };
 
-  if(action === 'list'){
-    $scope.get();
-  } else if(action === 'edit'){
-    $scope.initEditForm($routeParams.id);
-  } else if(action == 'new') {
-    $scope.cleanForm();
-  } else if(action == 'show'){
-    $scope.initShowPage($routeParams.id);
-  }
+    if(action === 'list'){
+      $scope.get();
+    } else if(action === 'edit'){
+      $scope.initEditForm($routeParams.id);
+    } else if(action == 'new') {
+      $scope.cleanForm();
+    } else if(action == 'show'){
+      $scope.initShowPage($routeParams.id);
+    }
 
-
-}]);
+  }]);
+})();
--- frontend/src/app/bookstoreapp.js
+++ frontend/src/app/bookstoreapp.js
@@ -1,44 +1,44 @@
-'use strict';
+(function(){
+  'use strict';
 
-var bookStoreApp = angular.module('bookStoreApp', ['ngRoute']);
-
-bookStoreApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/books', {
-        templateUrl: 'app/books/view/list.html',
-        controller: 'BookController',
-        resolve: {
-          action: function(){
-              return 'list';
+  angular.module('bookStoreApp', ['ngRoute']).config(['$routeProvider',
+    function($routeProvider) {
+      $routeProvider.
+        when('/books', {
+          templateUrl: 'app/books/view/list.html',
+          controller: 'BookController',
+          resolve: {
+            action: function(){
+                return 'list';
+            }
           }
-        }
-      }).
-      when('/books/new', {
-        templateUrl: 'app/books/view/create.html',
-        controller: 'BookController',
-        resolve: {
-          action: function(){
-              return 'new';
+        }).
+        when('/books/new', {
+          templateUrl: 'app/books/view/create.html',
+          controller: 'BookController',
+          resolve: {
+            action: function(){
+                return 'new';
+            }
           }
-        }
-      }).
-      when('/books/:id/edit', {
-        templateUrl: 'app/books/view/edit.html',
-        controller: 'BookController',
-        resolve: {
-          action: function(){
-              return 'edit';
+        }).
+        when('/books/:id/edit', {
+          templateUrl: 'app/books/view/edit.html',
+          controller: 'BookController',
+          resolve: {
+            action: function(){
+                return 'edit';
+            }
           }
-        }
-      }).
-      when('/books/:id/show', {
-        templateUrl: 'app/books/view/show.html',
-        controller: 'BookController',
-        resolve: {
-          action: function(){
-              return 'show';
+        }).
+        when('/books/:id/show', {
+          templateUrl: 'app/books/view/show.html',
+          controller: 'BookController',
+          resolve: {
+            action: function(){
+                return 'show';
+            }
           }
-        }
-      });
-  }]);
+        });
+    }]);
+})();
