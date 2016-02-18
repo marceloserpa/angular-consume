1bce58fb8be93945c504d16d93f4be2a9a3146d8
MarceloIlegra <marcelo.serpa@ilegra.com>
2016-02-18 16:41:44 -0200

    created config-routes file

--- frontend/src/app/bookstoreapp.js
+++ frontend/src/app/bookstoreapp.js
@@ -1,48 +1,6 @@
 (function(){
   'use strict';
 
-  angular.module('bookStoreApp', ['ngRoute']).config(['$routeProvider',
-    function($routeProvider) {
-      $routeProvider.
-        when('/books', {
-          templateUrl: 'app/books/view/list.html',
-          controller: 'BookController',
-          controllerAs: 'bookVm',
-          resolve: {
-            action: function(){
-                return 'list';
-            }
-          }
-        }).
-        when('/books/new', {
-          templateUrl: 'app/books/view/create.html',
-          controller: 'BookController',
-          controllerAs: 'bookVm',
-          resolve: {
-            action: function(){
-                return 'new';
-            }
-          }
-        }).
-        when('/books/:id/edit', {
-          templateUrl: 'app/books/view/edit.html',
-          controller: 'BookController',
-          controllerAs: 'bookVm',
-          resolve: {
-            action: function(){
-                return 'edit';
-            }
-          }
-        }).
-        when('/books/:id/show', {
-          templateUrl: 'app/books/view/show.html',
-          controller: 'BookController',
-          controllerAs: 'bookVm',
-          resolve: {
-            action: function(){
-                return 'show';
-            }
-          }
-        });
-    }]);
+  angular.module('bookStoreApp', ['ngRoute']);
+
 })();
---
+++ frontend/src/app/config-routes.js
@@ -0,0 +1,53 @@
+(function(){
+  'use strict'
+
+  angular
+    .module('bookStoreApp')
+    .config(ConfigureRoutes);
+
+  ConfigureRoutes.$inject = ['$routeProvider'];
+
+  function ConfigureRoutes($routeProvider) {
+    $routeProvider.
+      when('/books', {
+        templateUrl: 'app/books/view/list.html',
+        controller: 'BookController',
+        controllerAs: 'bookVm',
+        resolve: {
+          action: function(){
+              return 'list';
+          }
+        }
+      }).
+      when('/books/new', {
+        templateUrl: 'app/books/view/create.html',
+        controller: 'BookController',
+        controllerAs: 'bookVm',
+        resolve: {
+          action: function(){
+              return 'new';
+          }
+        }
+      }).
+      when('/books/:id/edit', {
+        templateUrl: 'app/books/view/edit.html',
+        controller: 'BookController',
+        controllerAs: 'bookVm',
+        resolve: {
+          action: function(){
+              return 'edit';
+          }
+        }
+      }).
+      when('/books/:id/show', {
+        templateUrl: 'app/books/view/show.html',
+        controller: 'BookController',
+        controllerAs: 'bookVm',
+        resolve: {
+          action: function(){
+              return 'show';
+          }
+        }
+      });
+  };
+})();
